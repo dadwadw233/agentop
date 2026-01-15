868b088391d4c5ac6d03aeb25bf83873a454eb25
yyh <913217005@qq.com>
2026-01-15 21:14:01 +0800

    Bump version to 0.1.1

--- agentop/__init__.py
+++ agentop/__init__.py
@@ -1,5 +1,5 @@
 """Agentop - Terminal UI for monitoring AI coding agents."""
 
-__version__ = "0.1.0"
-__author__ = "Your Name"
+__version__ = "0.1.1"
+__author__ = "Yuanhong Yu"
 __license__ = "MIT"
